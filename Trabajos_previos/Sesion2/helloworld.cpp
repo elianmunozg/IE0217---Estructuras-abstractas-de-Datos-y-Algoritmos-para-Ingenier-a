f1a4fa639be4bf916cc88a9280595d0862515e24
elianmunozg <eligo2001@gmail.com>
2024-01-04 12:03:49 -0600

    Prueba del operador ternario

--- Trabajos_previos/Sesion2/helloworld.cpp
+++ Trabajos_previos/Sesion2/helloworld.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main(){
     cout << "Hello World";
+
+    int var1=0;
+    int var2;
+
+    var2=(var1==0) ? 15 : 27;
+        
+    cout << "var2: \n" << var2 << "";
+
+
+
     return 0;
 }
