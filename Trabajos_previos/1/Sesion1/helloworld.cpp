d5adcb4e115f5edfaffaf70c9c1a3d9b2cf0474c
elianmunozg <eligo2001@gmail.com>
2024-01-04 23:08:35 -0600

    hello

---
+++ Trabajos_previos/1/Sesion1/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World";
+
+    int var1=0;
+    int var2;
+
+    var2=(var1==0) ? 15 : 27;
+        
+    cout << "var2: \n" << var2 << "";
+
+
+
+    return 0;
+}
