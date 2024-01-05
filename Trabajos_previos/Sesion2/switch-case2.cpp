2f3e5cc022cb63e914766c39adf57cb6810a4890
elianmunozg <eligo2001@gmail.com>
2024-01-04 20:26:01 -0600

    switch case 2

---
+++ Trabajos_previos/Sesion2/switch-case2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo, estudiantes de IE0237!\n";
+
+    int var = 1;
+    int var2 = 2;
+    int var3 = 3;
+
+    switch (var1) {
+        case 1:
+            std::cout << "Case 1!\n";
+            break;
+        case 2:
+        case 3:
+            std::cout << "Case Final!\n";
+            break;
+        
+        
+        default:
+            std::cout << "Default Case!\n";
+            break;
+    }
+
+    return 0;
+}
