cdb969ff5308b3ef2ac87156ea17ab16031045a3
elianmunozg <eligo2001@gmail.com>
2024-01-10 22:44:19 -0600

    Estructura b[asica del programa

---
+++ Tareas/1/game.hpp
@@ -0,0 +1,24 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+/**
+ * @file Game.hpp
+ * @brief Declaración de la clase Game para el juego de adivinanzas.
+ */
+
+class Game {
+public:
+    /**
+     * @brief Constructor de la clase Game.
+     */
+    Game();
+
+    /**
+     * @brief Inicia la ejecución del juego.
+     */
+    void run();
+
+    // Otros métodos y miembros privados aquí
+};
+
+#endif // GAME_HPP
---
+++ Tareas/1/main.cpp
@@ -0,0 +1,12 @@
+#include "Game.hpp"
+
+/**
+ * @brief Punto de entrada principal para el juego de adivinanzas.
+ * 
+ * @return int Código de salida del programa.
+ */
+int main() {
+    Game game;
+    game.run();
+    return 0;
+}
